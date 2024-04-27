2c549ec5c2e05c68849513ece64094ae5549cec6
Lord Frantex <lordfranktex@gmail.com>
2024-04-27 17:09:47 +0100

    added mrsAnaziah

--- src/app/page.js
+++ src/app/page.js
@@ -355,14 +355,14 @@ export default function Home() {
               </div>
 
             </div>
-            {/* <div className="col-md-3 ">
+            <div className="col-md-3 ">
               <div className="text-center team-card bg-white rounded-4 p-3">
                 <Image src='/pics/9967199.png' alt="" width={100} height={100} className="rounded-pill" />
-                <p>Mr. Thomson Mary</p>
-                <p>Doctor</p>
+                <p>Mrs. Ijeoma Anazia</p>
+                <p>Managing partner</p>
               </div>
 
-            </div> */}
+            </div>
             {/* <div className="col-md-3 ">
               <div className="text-center team-card bg-white rounded-4 p-3">
                 <Image src='/pics/4086652.png' alt="" width={100} height={100} className="rounded-pill" />
