47f96b1fa74c3f87a12ba351121c880ff7740540
Lord Frantex <lordfranktex@gmail.com>
2024-07-11 08:24:09 +0100

    updating

--- src/app/service/page.jsx
+++ src/app/service/page.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import { useState } from 'react';
 
-const page = () => {
+const Page = () => {
 
     const [file, setFile] = useState('');
 
@@ -52,4 +52,4 @@ const page = () => {
         </div>
     );
 }
-export default page
+export default Page
